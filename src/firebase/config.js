f4f9143a32322f8a671a69b112a02f386391445b
Sazzad Hossain <sharifh530@gmail.com>
2020-08-12 20:03:01 +0600

    Initialise firebase to store data on the firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCIjgtZO51UHF3M_WZlAb6C29-hh9JGmUg",
+  authDomain: "photo-galary-codershs.firebaseapp.com",
+  databaseURL: "https://photo-galary-codershs.firebaseio.com",
+  projectId: "photo-galary-codershs",
+  storageBucket: "photo-galary-codershs.appspot.com",
+  messagingSenderId: "771247932403",
+  appId: "1:771247932403:web:aa3787a4569612026fc886",
+  measurementId: "G-Z218TFJE7N",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
